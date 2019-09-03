48ac31e47b41e53bf0cb7b73bda046c5c72a2728
DannySuarez <suarezd10@gmail.com>
2019-09-03 15:37:37 -0700

    Scaffolded redux

---
+++ src/reducers/barsReducer.js
@@ -0,0 +1,12 @@
+
+
+const initialState = {
+  bars: []
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
+export default createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
